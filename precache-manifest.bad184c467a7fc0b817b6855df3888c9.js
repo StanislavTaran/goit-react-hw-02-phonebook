7a55a73a35eb9f7b55680a3622cd078c0a6b5f6d
Stanislav Taran <StanislavTaran@users.noreply.github.com>
2020-04-09 22:05:30 +0300

    Updates

---
+++ precache-manifest.bad184c467a7fc0b817b6855df3888c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7d6cded3cbcd959a86bac033da0fe8",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "9f0e4ce31ecd821634fb",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.b856c614.chunk.css"
+  },
+  {
+    "revision": "3c90c10370d8db26f0c2",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.775f4ed6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.775f4ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0e4ce31ecd821634fb",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.f11e4a59.chunk.js"
+  },
+  {
+    "revision": "4dc4691f794833de7ba9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.bd2cb211.js"
+  }
+]);
